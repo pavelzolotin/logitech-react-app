3e13178a311347d2ad4cc0b1f07428e608777b50
Pavel Zolotin <zolotinpavel@gmail.com>
2023-03-12 21:48:20 +0500

    add skeleton loading

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import WatchBlock from './components/WatchBlock';
+import Skeleton from './components/Skeleton';
 
 import './scss/app.scss';
 
 function App() {
     const [watches, setWatches] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchWatches = () => {
-        fetch('./watches.json')
+        fetch('https://6407307d862956433e676ec6.mockapi.io/items')
             .then(res => res.json())
-            .then(data => setWatches(data))
+            .then(data => {
+                setWatches(data);
+                setIsLoading(false);
+            })
             .catch(err => console.warn(err));
     };
 
     useEffect(() => {
         fetchWatches();
-    });
+    }, []);
 
     return (
         <div className="App">
@@ -34,12 +39,16 @@ function App() {
                         <h2 className="content__title">Все часы</h2>
                         <div className="content__items">
                             {
-                                watches.map(watch => (
-                                    <WatchBlock
-                                        key={watch.id}
-                                        {...watch}
-                                    />
-                                ))
+                                isLoading
+                                    ? [...Array(8)].map((_, i) => (
+                                        <Skeleton key={i}/>
+                                    ))
+                                    : watches.map(watch => (
+                                        <WatchBlock
+                                            key={watch.id}
+                                            {...watch}
+                                        />
+                                    ))
                             }
                         </div>
                     </div>
---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,18 @@
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+    <ContentLoader
+        speed={2}
+        width={280}
+        height={650}
+        viewBox="0 0 280 650"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+    >
+        <rect x="0" y="0" rx="3" ry="3" width="280" height="390" />
+        <rect x="0" y="447" rx="3" ry="3" width="280" height="100" />
+        <rect x="0" y="407" rx="3" ry="3" width="280" height="25" />
+    </ContentLoader>
+)
+
+export default Skeleton
