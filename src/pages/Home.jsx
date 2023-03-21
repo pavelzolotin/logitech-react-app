c7253ad2abd7247809bb1044e1cf3dcf750dced6
Pavel Zolotin <zolotinpavel@gmail.com>
2023-03-21 18:32:33 +0500

    add querystring library & get it in URL filters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import qs from 'qs';
 
-import {setCurrentPage} from '../redux/slices/filterSlice';
+import {sorts} from '../utils/constants';
+import {setCurrentPage, setFilters} from '../redux/slices/filterSlice';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import ItemBlock from '../components/ItemBlock';
@@ -10,36 +13,16 @@ import Skeleton from '../components/ItemBlock/Skeleton';
 import Pagination from '../components/Pagination';
 
 const Home = ({type}) => {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const {searchValue} = useSelector(state => state.search);
     const {categoryId, currentPage, orderType, sort} = useSelector(state => state.filter);
-    const sortType = sort.sortProperty;
-
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const isSearch = useRef(false);
+    const isMounted = useRef(false);
 
-    const onChangePage = (number) => {
-        dispatch(setCurrentPage(number));
-    };
-
-    useEffect(() => {
-        const fetchItems = () => {
-            setIsLoading(true);
-
-            const category = categoryId > 0 ? `category=${categoryId}` : '';
-            const search = searchValue ? `search=${searchValue}` : '';
-
-            axios.get(`https://6407307d862956433e676ec6.mockapi.io/${type}?page=${currentPage}&${category}${search}&sortBy=${sortType}&order=${orderType}`)
-                .then(res => {
-                    setProducts(res.data);
-                    setIsLoading(false);
-                })
-                .catch(err => console.warn(err));
-        };
-
-        fetchItems();
-        window.scrollTo(0, 0);
-    }, [searchValue, type, currentPage, categoryId, sortType, orderType]);
+    const sortType = sort.sortProperty;
 
     const items = products.map(item => (
         <ItemBlock
@@ -51,6 +34,62 @@ const Home = ({type}) => {
     const skeletons = [...Array(8)].map((_, i) => (
         <Skeleton key={i}/>
     ));
+
+    const onChangePage = (number) => {
+        dispatch(setCurrentPage(number));
+    };
+
+    useEffect(() => {
+        if (!isSearch.current) {
+            const fetchItems = () => {
+                setIsLoading(true);
+
+                const category = categoryId > 0 ? `category=${categoryId}` : '';
+                const search = searchValue ? `search=${searchValue}` : '';
+
+                axios.get(`https://6407307d862956433e676ec6.mockapi.io/${type}?page=${currentPage}&${category}${search}&sortBy=${sortType}&order=${orderType}`)
+                    .then(res => {
+                        setProducts(res.data);
+                        setIsLoading(false);
+                    })
+                    .catch(err => console.warn(err));
+            };
+
+            fetchItems();
+        }
+
+        isSearch.current = false;
+        window.scrollTo(0, 0);
+    }, [searchValue, type, currentPage, categoryId, sortType, orderType]);
+
+    useEffect(() => {
+        if (window.location.search) {
+            const params = qs.parse(window.location.search.substring(1));
+            const sort = sorts.find(obj => obj.sortProperty === params.sortProperty);
+
+            dispatch(
+                setFilters({
+                    ...params,
+                    sort
+                })
+            );
+            isSearch.current = true;
+        }
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (isMounted.current) {
+            const queryString = qs.stringify({
+                sortProperty: sortType,
+                categoryId,
+                currentPage
+            });
+
+            navigate(`?${queryString}`);
+        }
+
+        isMounted.current = true;
+    }, [searchValue, type, currentPage, categoryId, sortType, orderType, navigate]);
 
     return (
         <>
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -30,10 +30,15 @@ const filterSlice = createSlice({
         },
         setCurrentPage(state, action) {
             state.currentPage = action.payload;
+        },
+        setFilters(state, action) {
+            state.sort = action.payload.sort;
+            state.currentPage = Number(action.payload.currentPage);
+            state.categoryId = Number(action.payload.categoryId);
         }
     }
 });
 
-export const {setCategoryId, setSort, setOrderType, setIsVisible, setCurrentPage} = filterSlice.actions;
+export const {setCategoryId, setSort, setOrderType, setIsVisible, setCurrentPage, setFilters} = filterSlice.actions;
 
 export default filterSlice.reducer;
