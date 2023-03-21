45a9a9601055a033d74d788b6eef3f4abda97ee5
Pavel Zolotin <zolotinpavel@gmail.com>
2023-03-21 17:39:20 +0500

    fix Skeleton styles

--- src/components/ItemBlock/Skeleton.jsx
+++ src/components/ItemBlock/Skeleton.jsx
@@ -6,13 +6,13 @@ const Skeleton = () => (
         width={280}
         height={650}
         viewBox="0 0 280 650"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
+        backgroundColor="#bdbdbd"
+        foregroundColor="#c9c9c9"
     >
         <rect x="0" y="0" rx="3" ry="3" width="280" height="390" />
         <rect x="0" y="447" rx="3" ry="3" width="280" height="100" />
         <rect x="0" y="407" rx="3" ry="3" width="280" height="25" />
     </ContentLoader>
-)
+);
 
-export default Skeleton
+export default Skeleton;
